00038b51279fcb4ec914d2aa202cab914812ae09
gunbamm <56660732+gunbamm@users.noreply.github.com>
2023-10-15 22:04:39 +0900

    수정
    수정

---
+++ frontend/index.js
@@ -0,0 +1,18 @@
+const renderData = (data) => {
+    const main.document.querySelector("main");
+  data.forEach((obj) => {
+    const div = document.createElement("div");
+    div.innerText = obj.title;
+    main.appendChild(div);
+  }
+  
+  );
+};
+
+const fetchList = async () => {
+  const res = await fetch("/items");
+  const data = await res.json();
+  console.log(data);
+};
+
+fetchList();
--- frontend/write.js
+++ frontend/write.js
@@ -2,10 +2,12 @@ const form = document.getElementById("write-form");
 
 const handleSubmitForm = async (event) => {
   event.preventDefault();
+  const body = new FormData(form);
+  body.append("insetAt", new Date().getTime());
   try {
     const res = await fetch("/items", {
       method: "POST",
-      body: new FormData(form),
+      body,
     });
     const data = await res.json();
     if (data === "200") window.location.pathname = "/";
