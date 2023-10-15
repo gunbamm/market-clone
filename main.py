0be47a612195e6c9d2b2d01022a25cc4cac8f865
gunbamm <56660732+gunbamm@users.noreply.github.com>
2023-10-15 21:36:12 +0900

    수정
    수정

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from fastapi import FastAPI, UploadFile, Form
 from fastapi.staticfiles import StaticFiles
 from typing import Annotated
+import sqlite3
+
+con = sqlite3.connect('Market_DB.db', check_same_thread=False)
+cur = con.cursor()
 
 app = FastAPI()
 
 
 @app.post('/items')
-def create_item(image: UploadFile,
-                title: Annotated[str, Form()],
-                price: Annotated[int, Form()],
-                description: Annotated[str, Form()],
-                place: Annotated[str, Form()]):
-    print(image, title, price, description, place)
+async def create_item(image: UploadFile,
+                      title: Annotated[str, Form()],
+                      price: Annotated[int, Form()],
+                      description: Annotated[str, Form()],
+                      place: Annotated[str, Form()]):
+
+    image_bytes = await image.read()
+    cur.execute(f"""
+                INSERT INTO items(title,price,description,place)
+                VALUES ('{title}','{image_bytes.hex()}',{price},'{description}','{place}')
+                """)
+    con.commit()
     return '200'
 
 
